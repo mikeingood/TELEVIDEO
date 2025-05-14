6adba8b3f153f81edc3b5fb83c92eff60246c521
mikeingood <mikeingood@ya.ru>
2025-05-14 19:37:54 +0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,1 +1,14 @@
+import ffmpeg
+import os
+
+def convert_to_circle(input_path, output_path):
+    stream = ffmpeg.input(input_path)
+    stream = ffmpeg.output(
+        stream, output_path,
+        vf='scale=240:240:force_original_aspect_ratio=decrease,pad=240:240:(ow-iw)/2:(oh-ih)/2:color=black',
+        vcodec='libx264',
+        pix_fmt='yuv420p',
+        f='mp4'
+    )
+    ffmpeg.run(stream)
 
